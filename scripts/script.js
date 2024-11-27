20107a8b03c8a0a7724e87c63f8d5ad91a17962a
Szebenszki Bence <szebenszkib.2@gmail.com>
2024-11-27 16:21:29 +0100

    js

--- scripts/script.js
+++ scripts/script.js
@@ -4,32 +4,30 @@ const keyboardDiv = document.querySelector(".keyboard");
 const hangmanImage = document.querySelector(".hangman-box img");
 const gameModal = document.querySelector(".game-modal");
 const playAgainBtn = gameModal.querySelector("button");
+const hintText = document.querySelector(".hint-text b"); // Hint szöveg eleme
 
-
-let currentWord, correctLetters, wrongGuessCount;
-const maxGuesses = 6;
+let currentWord, currentHint, correctLetters, wrongGuessCount;
+const maxGuesses = 8;
 
 const resetGame = () => {
-
     correctLetters = [];
     wrongGuessCount = 0;
     hangmanImage.src = "images/hangman-0.svg";
     guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
     wordDisplay.innerHTML = currentWord.split("").map(() => `<li class="letter"></li>`).join("");
     keyboardDiv.querySelectorAll("button").forEach(btn => btn.disabled = false);
+    hintText.innerText = ""; // Tipp szöveg alaphelyzetbe állítása
     gameModal.classList.remove("show");
 }
 
 const getRandomWord = () => {
-   
     const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
     currentWord = word;
-    document.querySelector(".hint-text b").innerText = hint;
+    currentHint = hint;
     resetGame();
 }
 
 const gameOver = (isVictory) => {
-    
     const modalText = isVictory ? `You found the word:` : 'The correct word was:';
     gameModal.querySelector("img").src = `images/${isVictory ? 'victory' : 'lost'}.gif`;
     gameModal.querySelector("h4").innerText = isVictory ? 'Congrats!' : 'Game Over!';
@@ -38,29 +36,29 @@ const gameOver = (isVictory) => {
 }
 
 const initGame = (button, clickedLetter) => {
-    
-    if(currentWord.includes(clickedLetter)) {
-
+    if (currentWord.includes(clickedLetter)) {
         [...currentWord].forEach((letter, index) => {
-            if(letter === clickedLetter) {
+            if (letter === clickedLetter) {
                 correctLetters.push(letter);
                 wordDisplay.querySelectorAll("li")[index].innerText = letter;
                 wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
             }
         });
     } else {
-        
         wrongGuessCount++;
         hangmanImage.src = `images/hangman-${wrongGuessCount}.svg`;
+
+        // Tipp megjelenítése 3 rossz találat után
+        if (wrongGuessCount === 3) {
+            hintText.innerText = currentHint; // Hint megjelenítése
+        }
     }
-    button.disabled = true; 
+    button.disabled = true;
     guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
 
-
-    if(wrongGuessCount === maxGuesses) return gameOver(false);
-    if(correctLetters.length === currentWord.length) return gameOver(true);
+    if (wrongGuessCount === maxGuesses) return gameOver(false);
+    if (correctLetters.length === currentWord.length) return gameOver(true);
 }
-
 
 for (let i = 97; i <= 122; i++) {
     const button = document.createElement("button");
