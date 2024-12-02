4287f095539f92438550a88f7f6656aad78c03d4
Szebenszki <szebenszkib.2@gmail.com>
2024-12-02 13:55:57 +0100

    aaa

--- scripts/script.js
+++ scripts/script.js
@@ -4,70 +4,70 @@ const keyboardDiv = document.querySelector(".keyboard");
 const hangmanImage = document.querySelector(".hangman-box img");
 const gameModal = document.querySelector(".game-modal");
 const playAgainBtn = gameModal.querySelector("button");
+const hintText = document.querySelector(".hint-text b");
 
+let currentWord, currentHint, correctLetters, wrongGuessCount;
+const maxGuesses = 10;
 
-let currentWord, correctLetters, wrongGuessCount;
-const maxGuesses = 6;
+const letters = "aábcdeéfghiíjklmnoóöőpqrstuúüűvwxyz";
 
 const resetGame = () => {
-
     correctLetters = [];
     wrongGuessCount = 0;
     hangmanImage.src = "images/hangman-0.svg";
     guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
     wordDisplay.innerHTML = currentWord.split("").map(() => `<li class="letter"></li>`).join("");
     keyboardDiv.querySelectorAll("button").forEach(btn => btn.disabled = false);
+    hintText.innerText = "";
     gameModal.classList.remove("show");
 }
 
 const getRandomWord = () => {
-   
     const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
     currentWord = word;
-    document.querySelector(".hint-text b").innerText = hint;
+    currentHint = hint;
     resetGame();
 }
 
 const gameOver = (isVictory) => {
-    
-    const modalText = isVictory ? `You found the word:` : 'The correct word was:';
+    const modalText = isVictory ? `Kitaláltad:` : 'A helyes válasz:';
     gameModal.querySelector("img").src = `images/${isVictory ? 'victory' : 'lost'}.gif`;
-    gameModal.querySelector("h4").innerText = isVictory ? 'Congrats!' : 'Game Over!';
+    gameModal.querySelector("h4").innerText = isVictory ? 'Gratulálok!' : 'Nem talált!';
     gameModal.querySelector("p").innerHTML = `${modalText} <b>${currentWord}</b>`;
     gameModal.classList.add("show");
 }
 
 const initGame = (button, clickedLetter) => {
-    
-    if(currentWord.includes(clickedLetter)) {
-
+    if (currentWord.includes(clickedLetter)) {
         [...currentWord].forEach((letter, index) => {
-            if(letter === clickedLetter) {
+            if (letter === clickedLetter) {
                 correctLetters.push(letter);
                 wordDisplay.querySelectorAll("li")[index].innerText = letter;
                 wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
             }
         });
     } else {
-        
         wrongGuessCount++;
         hangmanImage.src = `images/hangman-${wrongGuessCount}.svg`;
+
+       
+        if (wrongGuessCount === 3) {
+            hintText.innerText = currentHint;
+        }
     }
-    button.disabled = true; 
+    button.disabled = true;
     guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
 
-
-    if(wrongGuessCount === maxGuesses) return gameOver(false);
-    if(correctLetters.length === currentWord.length) return gameOver(true);
+    if (wrongGuessCount === maxGuesses) return gameOver(false);
+    if (correctLetters.length === currentWord.length) return gameOver(true);
 }
 
-
-for (let i = 97; i <= 122; i++) {
+letters.split("").forEach(char => {
     const button = document.createElement("button");
-    button.innerText = String.fromCharCode(i);
+    button.innerText = char;
     keyboardDiv.appendChild(button);
-    button.addEventListener("click", (e) => initGame(e.target, String.fromCharCode(i)));
-}
+    button.addEventListener("click", (e) => initGame(e.target, char));
+});
 
 getRandomWord();
 playAgainBtn.addEventListener("click", getRandomWord);
---
+++ scripts/word-list.js
@@ -0,0 +1,76 @@
+const wordList = [
+    {
+        word: "gabona",
+        hint: "kenyeret készítenek belőle"
+    },
+    {
+        word: "alma",
+        hint: "minden nap egy ... az orvost távol tartja"
+    },
+    {
+        word: "monitor",
+        hint: "A számítógép perifériái közé tartozik"
+
+    },
+    {
+        word: "tanterem",
+        hint: "iskolai helyiség ahol a diákok tanulnak"
+    },
+    {
+        word: "kutya",
+        hint: "Háziállat, emlős, a Canidae családba tartozik"
+    },
+    {
+        word: "konyha",
+        hint: " A helyiség, ahol ételt készítenek."
+    },
+    {
+        word: "tavasz",
+        hint: "Az év egyik évszaka, a tél utáni időszak."
+    },
+    {
+        word: "Kihívás",
+        hint: "Olyan feladat, amely erőfeszítést, kitartást igényel, és nem könnyű teljesíteni"
+    },
+    {
+        word: "Szorgalom",
+        hint: "Kemény munka, kitartás, amit az ember arra fordít, hogy elérje céljait."
+    },
+    {
+        word: "Becsület",
+        hint: "Az erkölcsi tisztesség, amit az emberek saját maguknak és másoknak megőriznek."
+    }, 
+    {
+        word: "Alázat",
+        hint: " Az a tulajdonság, hogy valaki nem tekinti magát másoknál fontosabbnak, és tisztelettel viszonyul másokhoz."
+    },
+    {
+        word: "Táska",
+        hint: "Olyan eszköz, amelyet az emberek arra használnak, hogy kisebb dolgokat tartsanak benne és magukkal vigyék."
+    },
+    {
+        word: "Fáklya",
+        hint: "Olyan fényforrás, amelyet kézben lehet tartani, gyakran gyújtottak meg tábortüzeknél vagy ünnepeken."
+    },
+    {
+        word: "Zsák",
+        hint: "Egy olyan nagy, erős anyagból készült tartó, amelybe különféle dolgokat lehet tenni."
+    },
+    {
+        word: "Híd",
+        hint: "Olyan építmény, amely lehetővé teszi, hogy átjussunk egy vízfolyás, szakadék vagy más akadály fölött."
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    
+];
