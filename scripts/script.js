7156f1fa76b853b5aad8f06f3878a8c8ddc0d0f8
SzebenszkiBence <71698369+SzebenszkiBence@users.noreply.github.com>
2024-12-04 09:45:19 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -7,17 +7,14 @@ const playAgainBtn = gameModal.querySelector("button");
 const hintText = document.querySelector(".hint-text b");
 
 let currentWord, currentHint, correctLetters, wrongGuessCount;
-const maxGuesses = 10;
-
-const letters = "aábcdeéfghiíjklmnoóöőpqrstuúüűvwxyz";
-const maxGuesses = 10;
+const maxGuesses = 10; // Növeltük 11 képre (0-10)
 
 const resetGame = () => {
     correctLetters = [];
     wrongGuessCount = 0;
     hangmanImage.src = "images/hangman-0.svg";
     guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
-    hintText.innerText = "";
+    hintText.innerText = ""; // Tipp törlése
     wordDisplay.innerHTML = currentWord
         .split("")
         .map(() => `<li class="letter"></li>`)
@@ -34,12 +31,9 @@ const getRandomWord = () => {
 };
 
 const gameOver = (isVictory) => {
-    const modalText = isVictory ? `Kitaláltad:` : 'A helyes válasz:';
-    gameModal.querySelector("img").src = `images/${isVictory ? 'victory' : 'lost'}.gif`;
-    gameModal.querySelector("h4").innerText = isVictory ? 'Gratulálok!' : 'Nem talált!';
-    const modalText = isVictory ? `Eltaláltad:` : "A helyes válasz:";
+    const modalText = isVictory ? `You found the word:` : "The correct word was:";
     gameModal.querySelector("img").src = `images/${isVictory ? "victory" : "lost"}.gif`;
-    gameModal.querySelector("h4").innerText = isVictory ? "Gratulálok!" : "Game Over!";
+    gameModal.querySelector("h4").innerText = isVictory ? "Congrats!" : "Game Over!";
     gameModal.querySelector("p").innerHTML = `${modalText} <b>${currentWord}</b>`;
     gameModal.classList.add("show");
 };
@@ -57,6 +51,7 @@ const initGame = (button, clickedLetter) => {
         wrongGuessCount++;
         hangmanImage.src = `images/hangman-${wrongGuessCount}.svg`;
 
+        // Tipp megjelenítése 3 rossz találat után
         if (wrongGuessCount === 3) {
             hintText.innerText = currentHint;
         }
@@ -69,12 +64,16 @@ const initGame = (button, clickedLetter) => {
     if (correctLetters.length === currentWord.length) return gameOver(true);
 };
 
-letters.split("").forEach(char => {
-    const button = document.createElement("button");
-    button.innerText = char;
-    keyboardDiv.appendChild(button);
-    button.addEventListener("click", (e) => initGame(e.target, char));
-});
+// Generálás az ékezetes karakterekhez
+const generateKeyboard = () => {
+    const letters = "aábcdeéfghiíjklmnoóöőpqrstuúüűvwxyz";
+    letters.split("").forEach((char) => {
+        const button = document.createElement("button");
+        button.innerText = char;
+        keyboardDiv.appendChild(button);
+        button.addEventListener("click", (e) => initGame(e.target, char));
+    });
+};
 
 generateKeyboard();
 getRandomWord();
